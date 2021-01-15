273e80b632d24fb6277321e1af03124e7c330bf5
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 14:37:08 +0800

    Update DP10.c

--- DP10.c
+++ DP10.c
@@ -5,16 +5,15 @@ int main()
     long long num[50] = {0};
     int n,ncase;
 
-    num[1] = num[2] = 1;
-    for (int counter = 3;counter <= 45;counter++)
+    num[1] = num[2] = 1;   
+    for (int counter = 3;counter <= 45;counter++)   //計算Fibonacci數列(題目說到第45個
         num[counter] = num[counter - 2] + num[counter - 1];
 
-    scanf("%d",&ncase);
+    scanf("%d",&ncase);     
     while (ncase--)
     {
         scanf("%d",&n);
-        printf("%lld\n",num[n]);
+        printf("%lld\n",num[n]);    //印出數列中第n個數字
     }
-
     return 0;
 }
