33a2c9410b202e43e5a3021eba211560fa3371ef
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 16:29:29 +0800

    Update GD05.c

--- GD05.c
+++ GD05.c
@@ -12,14 +12,14 @@ int main()
     for (counter = 0;counter < job;counter++)
     {
         scanf("%d",&work_time);
-        for (min = 0,counter_one = 1;counter_one < machine;counter_one++)
+        for (min = 0,counter_one = 1;counter_one < machine;counter_one++)   //在machine個機器間選擇目前工作時間最少的給其工作
             if (time[counter_one] < time[min])
                 min = counter_one;
         time[min] += work_time;
     }
 
     for(max = 0,counter = 1;counter < machine;counter++)
-        if (time[max] < time[counter])
+        if (time[max] < time[counter])  //找出工作時間最長的那台機器
             max = counter;
-    printf("%d\n",time[max]);
+    printf("%d\n",time[max]);   
 }
