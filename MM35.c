9f696f68b65e8f30aac3087239311db606cc75cc
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 18:03:30 +0800

    Update MM35.c

--- MM35.c
+++ MM35.c
@@ -4,25 +4,25 @@ int main()
 {
     int element,pow;
     scanf("%d",&element);
-    while (scanf("%d",&element) == 1)
+    while (scanf("%d",&element) == 1)   //因為是一行一行輸出所以覆蓋前面的部分(不需要)
     {
-        for (int num = 2;element >= num;num++)
+        for (int num = 2;element >= num;num++)  //num需小於element(找element的因數)
         {
             pow = 0;
-            while (element % num == 0)
+            while (element % num == 0)  //若num為element的因數
             {
-                element /= num;
-                pow++;
+                element /= num; //除以num
+                pow++;  //該num的次方數+1
             }
             if (pow > 0)
             {
                 printf("%d",num);
-                if (pow > 1)
-                    printf("^%d",pow);
-                if (element > 1)
-                    printf("*");
-                else
-                    printf("\n");
+                if (pow > 1)    //若次方大於1
+                    printf("^%d",pow);  //印出次方數
+                if (element > 1)    //若element還未被整除
+                    printf("*");    //繼續找下一個因數
+                else    //若element已被整除
+                    printf("\n");   //換行
             }
         }
     }
