71c14ed48df8c9901fda15eeb62b442ede9296af
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 18:27:37 +0800

    Update MM73.c

--- MM73.c
+++ MM73.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    double temperature;
-    while (scanf("%lf",&temperature) == 1)
+    double temperature;	
+    while (scanf("%lf",&temperature) == 1)	//若偵測到一個輸入
     {
-        printf("Fahrenheit = %.2lf\n",temperature*((double)9 / (double)5) + 32);
-        printf("Absolute temperature = %.2lf\n",temperature + 273.15);
+        printf("Fahrenheit = %.2lf\n",temperature*((double)9 / (double)5) + 32);	//印出華氏的度數(攝氏 * (9/5) + 32 = 華氏)
+        printf("Absolute temperature = %.2lf\n",temperature + 273.15);	//印出絕對溫度度數(絕對溫度 = 攝氏 * 273.15)
     }
     return 0;
 }
