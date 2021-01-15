dafb6970a06cf482e0ca5d18fb08372e683e0595
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 13:21:51 +0800

    Update AR23.c

--- AR23.c
+++ AR23.c
@@ -5,14 +5,14 @@
 int main()
 {
     char input[10000];
-    while (fgets(input,10000,stdin))
+    while (fgets(input,10000,stdin))   
     {
         int len = strlen(input);
         for (int counter = 0;counter < len;counter++)
         {
-            if (isalpha(input[counter]))
+            if (isalpha(input[counter]))    //確認是否為字母
             {
-                if ((input[counter]>='A' && ((input[counter] + 13) <= 'Z')) || (input[counter] >= 'a' && ((input[counter] + 13) <= 'z')))
+                if ((input[counter]>='A' && ((input[counter] + 13) <= 'Z')) || (input[counter] >= 'a' && ((input[counter] + 13) <= 'z')))   //如果該字母在轉換後仍是字母(在ASCIIASCII
                     printf("%c",input[counter] + 13);
                 else
                     printf("%c",input[counter] + 13 - 26);
