adc1ff7cbad2ab4a7d64c1abb9647f74e0bded6e
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 18:11:43 +0800

    Update MM47.c

--- MM47.c
+++ MM47.c
@@ -3,12 +3,13 @@
 int main()
 {
     int adult_num,adult_fare,children_num,children_fare,senior_num,senior_fare;
-
+    
+    //依序讀取人數及所需費用
     scanf("%d%d",&adult_num,&adult_fare);
     scanf("%d%d",&children_num,&children_fare);
     scanf("%d%d",&senior_num,&senior_fare);
 
-    int sum = adult_num * adult_fare + children_num * children_fare + senior_num * senior_fare;
+    int sum = adult_num * adult_fare + children_num * children_fare + senior_num * senior_fare; //計算總費用
     printf("%d\n",sum);
 
     return 0;
