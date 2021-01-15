ff10a801a57462159e3b100f78f9b871a63e0c4f
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 17:42:47 +0800

    Update MM24.c

--- MM24.c
+++ MM24.c
@@ -3,7 +3,7 @@
 int main()
 {
     long input_one,input_two;
-    while (scanf("%ld%ld",&input_one,&input_two) == 2)
-        printf("%ld\n",input_one - input_two > 0 ? (input_one - input_two) : (input_two - input_one));
+    while (scanf("%ld%ld",&input_one,&input_two) == 2)  //若偵測到有2個輸入後
+        printf("%ld\n",input_one - input_two > 0 ? (input_one - input_two) : (input_two - input_one));  //判斷input_one - input_two是否大於0，是則輸出input_one - input_two，否則反之
     return 0;
 }
