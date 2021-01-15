b07d6674402dca1facbb817834446e9f8d73cbeb
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 18:40:28 +0800

    Update SO03.c

--- SO03.c
+++ SO03.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #define NUM 10010
 
-int compare(const void *a,const void *b)
+int compare(const void *a,const void *b)    //用於qsort的比較函式
 {
     return (*(int*)a - *(int*)b);
 }
@@ -12,28 +12,32 @@ int main()
     {
         int num,enemy[NUM],solder[NUM],count = 0,counter,counter_one;
         scanf("%d",&num);
-        if (num == 0)
-            break;
+        if (num == 0)   //若輸入為0
+            break;  //停止輸出
         for (counter = 0;counter < num;counter++)
             scanf("%d",enemy + counter);
         for(counter = 0;counter < num;counter++)
             scanf("%d",solder + counter);
-        qsort(enemy,num,sizeof(int),compare);
-        qsort(solder,num,sizeof(int),compare);
+        qsort(enemy,num,sizeof(int),compare);   //將enemy以力量值由小至大排序
+        qsort(solder,num,sizeof(int),compare);  //將solder以力量值由小至大排序
+        
+        //計數器歸零
         counter = 0;
         counter_one = 0;
         while (counter_one < num)
         {
-            if (enemy[counter] < solder[counter_one])
+            if (enemy[counter] < solder[counter_one])   //若enemy的力量值小於solder
             {
-                count++;
-                counter++;
+                count++;    //勝利數+1
+                
+                //繼續往後比較(若是之後的enemy亦可被現在的solder擊敗那也可以被更強的solder擊敗)
+                counter++;  
                 counter_one++;
             }
-            else
+            else    //若此場未奪勝則繼續找更強的solder
                 counter_one++;
         }
-        printf("%d\n",count);
+        printf("%d\n",count);   //印出此次勝利數
     }
     return 0;
 }
