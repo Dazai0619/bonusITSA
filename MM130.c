0ce9c544aed58abc40672eb39d9ccb9208aa5e32
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 17:14:55 +0800

    Update MM130.c

--- MM130.c
+++ MM130.c
@@ -12,11 +12,11 @@ int main()
     int counter = 0;
     while (scanf("%d",&num[counter]) != EOF)
         counter++;
-    qsort(num,counter,sizeof(num[0]),cmp);
+    qsort(num,counter,sizeof(num[0]),cmp);  //由小排到大
 
-    if (counter % 2 != 0)
+    if (counter % 2 != 0)   //若共有奇數個數字
         printf("%d\n",num[(counter - 1) / 2]);
-    else
+    else    //若共有偶數個數字
     {
         float mid = (num[(counter / 2) - 1] + num[(counter / 2)]) / 2 ;
         printf("%f\n",mid);
