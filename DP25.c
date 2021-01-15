e77e291e7d2ce4e863dbe7a26cfd38e720c0b5e5
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 15:29:55 +0800

    Update DP25.c

--- DP25.c
+++ DP25.c
@@ -8,11 +8,11 @@ int main()
     num[0] = 1;
     num[1] = num[2] = 2;
 
-    for(counter = 3;counter <= 90;counter++)
+    for(counter = 3;counter <= 90;counter++)    //將題目指定範圍之數列計算出來，建立該遞迴函式之解
         num[counter] = num[counter - 1] / 4 + num[counter - 2] / 2 + num[counter - 3];
 
-    while(scanf("%d",&input) == 1)
-        printf("%lld\n",num[input]);
+    while(scanf("%d",&input) == 1)  //若偵測到1個輸入
+        printf("%lld\n",num[input]);    //印出
 
     return 0;
 }
