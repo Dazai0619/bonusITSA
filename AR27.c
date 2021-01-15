f16d6214c799d93191e355767f798b04bd0751dd
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 13:45:16 +0800

    Update AR27.c

--- AR27.c
+++ AR27.c
@@ -6,21 +6,25 @@
 int main()
 {
     int num,tmp,first = 1;
-    bool check[100000] = {0};
+    bool check[100000] = {0};   
 
     scanf("%d",&num);
     while (scanf("%d",&tmp) == 1)
-        check[tmp] = true;
+        check[tmp] = true;  //標記目前有的
     for (int counter = 1;counter <= num;counter++)
     {
-        if (!check[counter])
+        if (!check[counter])	//如果沒有	
         {
-            if(first)
+            if(first)	//如果是奇數個：不印空格
+            {
                 printf("%d",counter);
-            else
+				first = 0;
+			}
+            else	//如果是偶數個：印空格
+            {
                 printf(" %d",counter);
-
-            first = 0;
+				first = 1;
+			}            
         }
     }
     printf("\n");
