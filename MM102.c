086cb0eb4a9a59a2cdb32ad7d5621e0d66509b40
Dazai0619 <58171799+Dazai0619@users.noreply.github.com>
2021-01-15 17:04:08 +0800

    Update MM102.c

--- MM102.c
+++ MM102.c
@@ -5,22 +5,22 @@ int main()
     int narray[20000],num,temp,first = 0,counter,counter_one;
     while (scanf("%d",&num) == 1)
     {
-        if (num == 0)
-            break;
+        if (num == 0)   //若無輸入
+            break;  //結束
 
-        if (first)
-            printf("\n");
-        else
+        if (first)  //如果該行已輸出完畢
+            printf("\n");   //換行
+        else    //剛進迴圈時不換行
             first = 1;
 
         for (counter = 0;counter < num;counter++)
             scanf("%d",&(narray[counter]));
 
-        for(counter = 0;counter < num - 1;counter++)
+        for(counter = 0;counter < num - 1;counter++)    //進行bubble sort    
         {
             for(counter_one = 0;counter_one < num - counter - 1;counter_one++)
             {
-                if (narray[counter_one + 1] > narray[counter_one])
+                if (narray[counter_one + 1] > narray[counter_one])  //將位於後面且較大的數字移到前面
                 {
                     temp = narray[counter_one];
                     narray[counter_one] = narray[counter_one + 1];
@@ -30,7 +30,9 @@ int main()
         }
 
         for (counter = 0;counter < num;counter++)
-            printf("%d %d\n",(counter + 1),narray[counter]);
+            printf("%d %d\n",(counter + 1),narray[counter]);    //印出排序好的index跟數字
+        
+        first = 0;  //歸零換行flag
     }
     return 0;
 }
